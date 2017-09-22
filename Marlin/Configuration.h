086d6c8aef5b726dafe1511a7031624067b75c83
MichaelG <michael@groene.de>
2017-09-23 01:01:19 +0200

    changed steps_per_unit for y-axis

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -524,7 +524,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 32, 106.103, 8.8888 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 106.103, 8.8888 }
 
 /**
  * Default Max Feed Rate (mm/s)
