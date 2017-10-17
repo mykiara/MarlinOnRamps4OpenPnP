caf39c006c7fdea5016001912e23fe48849e3ad3
MichaelG <michael@groene.de>
2017-10-17 17:35:27 +0200

    +offset adapted to new head

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -680,7 +680,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER 0  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 0  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -2   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER 0.34   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 6000
